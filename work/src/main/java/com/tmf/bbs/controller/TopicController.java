d8b73dfc61b75014be2c8fa24645d156f2093696
伍舟 <108986142+mylelouch@users.noreply.github.com>
2022-09-10 16:59:41 +0800

    Update TopicController.java

--- work/src/main/java/com/tmf/bbs/controller/TopicController.java
+++ work/src/main/java/com/tmf/bbs/controller/TopicController.java
@@ -23,8 +23,8 @@ public class TopicController {
 	//发帖
 	@RequestMapping("/topic_addTopic.do")
 	@ResponseBody
-	public String add(HttpServletRequest request, HttpServletResponse response,String title,String content,Integer topics_user_id){
-       services.addTopic(request,response,title,content,topics_user_id);
+	public String add(HttpServletRequest request, HttpServletResponse response,String title,String content,Integer topics_user_id,String password){
+       services.addTopic(request,response,title,content,topics_user_id,password);
 	   return "add successfully";
 	}
 	//删帖
@@ -59,8 +59,8 @@ public class TopicController {
 	//更新帖子
 	@RequestMapping("/updateTopic.do")
 	@ResponseBody
-	public String update(Integer id,Integer user_id,String content){
-		services.updateTopic(id,user_id,content);
+	public String update(Integer id,Integer topics_user_id,String content,String password){
+		services.updateTopic(id,topics_user_id,content,password);
 		return "update successfully";
 	}
 }
